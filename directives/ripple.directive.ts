971d8501964773b716fb8a151bd7e1d71c419eb9
Naveed Hassan <08es34@gmail.com>
2023-11-19 19:11:11 +0500

    fixed ripple

--- directives/ripple.directive.ts
+++ directives/ripple.directive.ts
@@ -25,7 +25,10 @@ function createRipple(event: any) {
 
   // fix: reset the overflow AFTER the ripple is done animating
   const { animationDuration } = getComputedStyle(circle)
-  button.$timeout = setTimeout(() => button.style.overflow = overflow, parseFloat(animationDuration) * 1000)
+  button.$timeout = setTimeout(() => {
+    button.style.overflow = overflow
+    circle.remove()
+  }, parseFloat(animationDuration) * 1000)
 }
 
 export const vRipple = {
