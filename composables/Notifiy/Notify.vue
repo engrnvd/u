42bb2f03c3a4faa4cf8a16c8a704f79253d3a60c
Naveed Hassan <08es34@gmail.com>
2024-01-08 19:22:38 +0500

    fix

--- composables/Notifiy/Notify.vue
+++ composables/Notifiy/Notify.vue
@@ -1,18 +1,19 @@
 <template>
     <div :class="{'notification-container-empty' : items.length===0}"
-         class="fixed top-0 right-0 z-50 w-72 px-4 max-h-screen">
+         class="fixed top-0 right-0 z-50 max-h-screen px-4">
         <TransitionGroup class="relative" name="list-slide" tag="div">
             <div v-for="item in items" :key="item.id"
-                 class="rounded cursor-pointer mt-4 text-xs bg-bg overflow-hidden"
+                 class="mt-4 w-64 cursor-pointer overflow-hidden rounded text-xs bg-bg"
                  @click="removeItem(item)">
-                <div :class="{[`bg-${item.options.type}`]: true, [getTextClass(item.options.type)]: true}"
-                     class="w-full shadow flex items-center space-x-2 p-4 ">
-                    <div class="all-center text-2xl">
+                <div
+                    :class="{[`bg-${item.options.type}`]: true, [getTextClass(item.options.type)]: true}"
+                    class="flex w-full items-center p-4 shadow space-x-2">
+                    <div class="text-2xl all-center">
                         <CheckCircleIcon v-if="item.options.type === 'success'"/>
                         <AlertCircleIcon v-else-if="item.options.type === 'danger'"/>
                         <InformationIcon v-else/>
                     </div>
-                    <div class="w-full">
+                    <div class="flex-grow">
                         <h4 v-if="item.title" class="mb-1">{{ item.title }}</h4>
                         <div v-if="item.message" v-html="item.message"/>
                     </div>
