82b205321594be5bcc372430ad4f7c7bf29b1317
Naveed Hassan <08es34@gmail.com>
2024-01-08 19:11:08 +0500

    fixed background problem

--- composables/Notifiy/Notify.vue
+++ composables/Notifiy/Notify.vue
@@ -3,17 +3,19 @@
          class="fixed top-0 right-0 z-50 w-72 px-4 max-h-screen">
         <TransitionGroup class="relative" name="list-slide" tag="div">
             <div v-for="item in items" :key="item.id"
-                 :class="{[`bg-${item.options.type}`]: true, [getTextClass(item.options.type)]: true}"
-                 class="shadow flex items-center space-x-2 p-4 rounded cursor-pointer mt-4 text-xs"
+                 class="rounded cursor-pointer mt-4 text-xs bg-bg overflow-hidden"
                  @click="removeItem(item)">
-                <div class="all-center text-2xl">
-                    <CheckCircleIcon v-if="item.options.type === 'success'"/>
-                    <AlertCircleIcon v-else-if="item.options.type === 'danger'"/>
-                    <InformationIcon v-else/>
-                </div>
-                <div class="w-full">
-                    <h4 v-if="item.title" class="mb-1">{{ item.title }}</h4>
-                    <div v-if="item.message" v-html="item.message"/>
+                <div :class="{[`bg-${item.options.type}`]: true, [getTextClass(item.options.type)]: true}"
+                     class="w-full shadow flex items-center space-x-2 p-4 ">
+                    <div class="all-center text-2xl">
+                        <CheckCircleIcon v-if="item.options.type === 'success'"/>
+                        <AlertCircleIcon v-else-if="item.options.type === 'danger'"/>
+                        <InformationIcon v-else/>
+                    </div>
+                    <div class="w-full">
+                        <h4 v-if="item.title" class="mb-1">{{ item.title }}</h4>
+                        <div v-if="item.message" v-html="item.message"/>
+                    </div>
                 </div>
             </div>
         </TransitionGroup>
