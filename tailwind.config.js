499e4a3a19ced26e13e3e256d5427ddd6922fb59
Naveed Hassan <08es34@gmail.com>
2024-01-08 20:20:07 +0500

    fixed shadow in dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,7 +56,7 @@ module.exports = {
                 overlay: 'var(--overlay)'
             },
             boxShadow: {
-                DEFAULT: '0 2px 2px 0 rgb(0 0 0 / 0.2), 0 1px 5px 0 rgb(0 0 0 / 0.1)'
+                DEFAULT: 'var(--shadow)'
             },
             backgroundColor: {
                 DEFAULT: 'var(--bg)',
